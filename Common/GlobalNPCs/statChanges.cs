74bf19d96aef0adeee593a2f426909fa0d0cb605
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-06 17:03:23 -0400

    Update statChanges.cs

--- Common/GlobalNPCs/statChanges.cs
+++ Common/GlobalNPCs/statChanges.cs
@@ -30,7 +30,7 @@ namespace prefixtest.Common.GlobalNPCs
 		{
 				// Main.NewText($"{npc.GivenName}  {npc.FullName} {npc.getName()}");
         Random random = new Random();
-        int roll2 = random.Next(19, 20); // creates a number from 1 to n-1
+        int roll2 = random.Next(1, 20); // creates a number from 1 to n-1
         switch (roll2){
 
           case 1:
