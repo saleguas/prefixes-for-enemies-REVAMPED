b76b1095b47b7feccee1cf308cb6e20e401c884c
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-08 15:03:27 -0400

    Update GhastlyKalis.cs

--- Items/Tokens/tier1/GhastlyKalis.cs
+++ Items/Tokens/tier1/GhastlyKalis.cs
@@ -10,7 +10,7 @@ namespace prefixtest.Items.Tokens.tier1 {
   public class GhastlyKalis: ModItem {
     public override void SetStaticDefaults() {
       DisplayName.SetDefault("Ghastly Kalis"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-      Tooltip.SetDefault("Grants extra invincibility frames on hit!");
+      Tooltip.SetDefault("Grants invincibility on hit");
     }
 
     public override void SetDefaults() {
@@ -31,8 +31,8 @@ namespace prefixtest.Items.Tokens.tier1 {
     }
     // This method gets called when firing your weapon/sword.
     public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit) {
-         player.AddBuff(BuffID.Invisibility, 300);
-         player.AddBuff(BuffID.ShadowDodge, 300);
+         player.AddBuff(BuffID.Invisibility, 5);
+         player.AddBuff(BuffID.ShadowDodge, 5);
     }
 
 
