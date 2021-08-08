386fc4edba557cc79cc51ec7ec7a2dc44d953132
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-08 14:29:28 -0400

    Ghastly Kalis

---
+++ Items/Tokens/tier1/GhastlyKalis.cs
@@ -0,0 +1,43 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using System;
+
+
+namespace prefixtest.Items.Tokens.tier1 {
+  public class GhastlyKalis: ModItem {
+    public override void SetStaticDefaults() {
+      DisplayName.SetDefault("Ghastly Kalis"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+      Tooltip.SetDefault("Grants extra invincibility frames on hit!");
+    }
+
+    public override void SetDefaults() {
+      Item.width = 24;
+      Item.height = 12;
+      Item.scale *= 1.5f;
+
+      Item.useStyle = ItemUseStyleID.Stabbing;
+      Item.useTime = 13;
+      Item.useAnimation = 20;
+      Item.autoReuse = true;
+      Item.noMelee = false;
+
+      Item.DamageType = DamageClass.Melee;
+      Item.damage = 35;
+      Item.knockBack = 0;
+      Item.crit = 45;
+      public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit) {
+			     player.AddBuff(BuffID.Regenerating, 60);
+		}
+    }
+    // This method gets called when firing your weapon/sword.
+
+
+
+
+
+
+  }
+}
