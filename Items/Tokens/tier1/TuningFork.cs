62ae37e925c2998e5c3be1674fc68d5b6f94756b
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-08 17:33:27 -0400

    Update TuningFork.cs

--- Items/Tokens/tier1/TuningFork.cs
+++ Items/Tokens/tier1/TuningFork.cs
@@ -13,25 +13,26 @@ namespace prefixtest.Items.Tokens.tier1{
 		}
 
 		public override void SetDefaults() {
-			Item.width = 40; //The item texture's width.
-			Item.height = 40; //The item texture's height.
-
-			Item.useStyle = ItemUseStyleID.Swing; // The useStyle of the Item.
-			Item.useTime = 20; // The time span of using the weapon. Remember in terraria, 60 frames is a second.
-			Item.useAnimation = 20; // The time span of the using animation of the weapon, suggest setting it the same as useTime.
-			Item.autoReuse = true; // Whether the weapon can be used more than once automatically by holding the use button.
+			Item.width = 40;
+			Item.height = 40;
+			Counter = 0;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 13;
+			Item.useAnimation = 13;
 			Item.noMelee = false;
 
-			Item.DamageType = DamageClass.Melee; //Whether your item is part of the melee class.
-			Item.damage = 69; //The damage your item deals.
-			Item.knockBack = 3; //The force of knockback of the weapon. Maximum is 20
-			Item.crit = 28; //The critical strike chance the weapon has. The player, by default, has a 4% critical strike chance.
-
-			Item.value = Item.buyPrice(gold: 35); //The value of the weapon in copper coins.
-			Item.rare = ModContent.Yellow; // Give this item our custom rarity.
-			Item.UseSound = SoundID.Item1; //The sound when the weapon is being used.
+			Item.DamageType = DamageClass.Melee;
+			Item.damage = 69;
+			Item.knockBack = 3;
+			Item.crit = 28;
+			Item.value = Item.buyPrice(gold: 35);
+			Item.rare = ModContent.Yellow;
+			Item.UseSound = SoundID.Item1;
 		}
-
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit) {
+         Counter++;
+    }
+		//when right click fire music notes , amount depending on counter projectile name is "TiedEighthNote" fire like a shotgun
 
 	}
 }
