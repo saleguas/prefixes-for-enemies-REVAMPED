6930cc17df86bb345ece534a19405770ad8939ac
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-08 17:30:24 -0400

    tuning fork

--- Items/Tokens/tier1/GhastlyKalis.cs
+++ Items/Tokens/tier1/GhastlyKalis.cs
@@ -10,23 +10,26 @@ namespace prefixtest.Items.Tokens.tier1 {
   public class GhastlyKalis: ModItem {
     public override void SetStaticDefaults() {
       DisplayName.SetDefault("Ghastly Kalis"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-      Tooltip.SetDefault("Grants invincibility on hit");
+      Tooltip.SetDefault("Grants invincibility and invisiblity while hitting enemies! However, it has very short range");
     }
 
     public override void SetDefaults() {
-      Item.width = 32;
-      Item.height = 32;
-      Item.scale *= 2.0f;
+      Item.width = 24;
+      Item.height = 12;
+      Item.scale *= 1.5f;
 
       Item.useStyle = 3;
       Item.useTime = 4;
       Item.useAnimation = 5;
       Item.autoReuse = true;
 
+      Item.value = Item.buyPrice(gold: 35);
+      Item.rare = ItemRarityID.Pink;
+
       Item.DamageType = DamageClass.Melee;
-      Item.damage = 35;
+      Item.damage = 45;
       Item.knockBack = 0;
-      Item.crit = 45;
+      Item.crit = 100;
 
     }
     // This method gets called when firing your weapon/sword.
