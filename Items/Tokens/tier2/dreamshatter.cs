3f54637fb0a4aee6890a3b09cab1525f30dec5ff
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-10 00:01:28 -0400

    Update dreamshatter.cs

--- Items/Tokens/tier2/dreamshatter.cs
+++ Items/Tokens/tier2/dreamshatter.cs
@@ -30,8 +30,8 @@ namespace prefixtest.Items.Tokens.tier2
 
 			// Weapon Properties
 			Item.DamageType = DamageClass.Ranged; // Sets the damage type to ranged.
-			Item.damage = 20; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
-			Item.knockBack = 5f; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
+			Item.damage = 42; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			Item.knockBack = .2f; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			Item.noMelee = true; // So the item's animation doesn't do damage.
 
 			// Gun Properties
