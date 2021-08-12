b4c746c2e09c4c7ce04a355303a32611f29982fb
Bricelyns <77311944+Bricelyns@users.noreply.github.com>
2021-08-12 16:50:44 -0400

    Update zbLoot.cs

--- Common/GlobalNPCs/zbLoot.cs
+++ Common/GlobalNPCs/zbLoot.cs
@@ -49,12 +49,71 @@ namespace prefixtest.Common.GlobalNPCs
         return;
 
       if(prefix.Contains("Burning")){
-        if(Main.rand.Next(4) == 0) // 1 in 5 chance, 20% drop chance
+        if(Main.rand.Next(3) == 0) // 1 in 2 chance, 50% drop chance
           // don't touch any parameters besides the last 2
           // ItemID.Hellstone means to drop Hellstone, find list of IDS here https://terraria.fandom.com/wiki/Item_IDs
           // Math.rand.Next(1, 10) means that to drop 1-9 hellstne, Math.rand.Next gives a number between(n, n-1)
         	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.Hellstone, Main.rand.Next(1, 10));
       }
+			if(prefix.Contains("Mythical")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.BrokenHeroSword, 1);
+      }
+			if(prefix.Contains("Steadfast")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.CobaltShield, 1);
+      }
+			if(prefix.Contains("Juggernaut")){
+        if(Main.rand.Next(4) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.PaladinsShield, 1);
+				if(Main.rand.Next(4) == 0)
+					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.FleshKnuckles, 1);
+      }
+			if(prefix.Contains("Tough")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.Shackle, 1);
+      }
+			if(prefix.Contains("Sus")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.WhoopieCushion, 1);
+      }
+			if(prefix.Contains("Placid")){
+        if(Main.rand.Next(2) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.CalmingPotion, Main.rand.Next(1, 4));
+      }
+			if(prefix.Contains("Wealthy")){
+        if(Main.rand.Next(2) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.MoneyTrough, 1);
+      }
+			if(prefix.Contains("Cool")){
+        if(Main.rand.Next(2) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.AviatorSunglasses, 1);
+      }
+			if(prefix.Contains("Frozen")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.FrozenTurtleShell, 1);
+				if(Main.rand.Next(2) == 0)
+	        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.WarmthPotion, Main.rand.Next(1, 4));
+      }
+			if(prefix.Contains("Dangerous")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.AvengerEmblem, 1);
+      }
+			if(prefix.Contains("Dangerous")){
+        if(Main.rand.Next(3) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.AvengerEmblem, 1);
+      }
+			if(prefix.Contains("Wing Clipper")){
+        if(Main.rand.Next(2) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.SoulofFlight, Main.rand.Next(1, 10));
+      }
+			if(prefix.Contains("Venemous")){
+        if(Main.rand.Next(2) == 0)
+        	Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.FlaskofVenom, Main.rand.Next(1, 4));
+      }
+
+
+			
 
 
 			//TODO: Add the rest of the vanilla drop rules!!
