484f60681df7be4a57f451461867654e3ca65865
Sal <salvadoraleguas@gmail.com>
2023-03-14 03:11:22 -0400

    Update LuckyBuff.cs

--- luckyblocks/Buffs/LuckyBuff.cs
+++ luckyblocks/Buffs/LuckyBuff.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -25,23 +28,105 @@ namespace luckyblocks.Buffs
             if (timer <= 0)
             {
                 timer = 300; // Reset the timer to 300 when the dust is inactive
-                int type = NPCID.Plantera;
 
-                if (Main.netMode != NetmodeID.MultiplayerClient)
-                {
-                    // If the player is not in multiplayer, spawn directly
-                    NPC.SpawnOnPlayer(player.whoAmI, type);
-                }
-                else
-                {
-                    // If the player is in multiplayer, request a spawn
-                    // This will only work if NPCID.Sets.MPAllowedEnemies[type] is true, which we set in this class above
-                    NetMessage.SendData(MessageID.SpawnBoss, number: player.whoAmI, number2: type);
-                }
+                // SpawnRandomBoss(player);
+                ChangeTime(player);
 
                 player.ClearBuff(ModContent.BuffType<LuckyBuff>());
 
             }
 		}
+
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+        /*                                                                    GOOD EVENTS                                                                   */
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+        /*                                                                    MISC EVENTS                                                                   */
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+        public void MakeItSlimeRain(Player player){
+            Main.NewText("It's slime raining!", Color.Yellow);
+
+            Main.StartSlimeRain();
+        }
+
+        public void MakeItRain(Player player){
+            Main.NewText("It's raining!", Color.Green);
+            Main.StartRain();
+        }
+        
+        public void ChangeTime(Player player){
+            Main.NewText("The time has changed!", Color.Cyan);
+
+            // pick random number between 1 and 4
+            int randomTime = Main.rand.Next(0, 54000);
+
+            Main.time = randomTime;
+
+        }
+
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+        /*                                                                BAD EVENTS                                                               */
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+
+
+
+
+        public void MakeInvasionHappen(Player player){
+            Main.NewText("You hear footsteps in the distance!", Color.Red);
+            // Goblin army is 1
+            // Frost legion is 2
+            // Pirates is 3
+            // Martian madness is 4
+
+            // pick random number between 1 and 4
+            int randomInvasion = Main.rand.Next(1, 5);
+
+            Main.StartInvasion(randomInvasion);
+        }
+
+        public void SpawnRandomBoss(Player player)
+        {
+            Main.NewText("An ancient force has awoken!", Color.Red);
+
+            // make a list of integer IDS for all bosses
+
+            List<int> bossList = new List<int>{
+                NPCID.KingSlime,
+                NPCID.EyeofCthulhu,
+                NPCID.EaterofWorldsHead,
+                NPCID.BrainofCthulhu,
+                NPCID.QueenBee,
+                NPCID.SkeletronHead,
+                // NPCID.WallofFlesh,
+                NPCID.QueenSlimeBoss,
+                NPCID.TheDestroyer,
+                NPCID.SkeletronPrime,
+                NPCID.Retinazer,
+                NPCID.Spazmatism,
+                NPCID.Deerclops,
+                NPCID.Plantera,
+                NPCID.Golem,
+                NPCID.DukeFishron,  
+                NPCID.CultistBoss,
+                NPCID.MoonLordCore,
+            };
+
+            // randomly select an item from the list
+            int randomBoss = bossList[Main.rand.Next(bossList.Count)];
+            
+
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                // If the player is not in multiplayer, spawn directly
+                NPC.SpawnOnPlayer(player.whoAmI, randomBoss);
+            }
+            else
+            {
+                // If the player is in multiplayer, request a spawn
+                // This will only work if NPCID.Sets.MPAllowedEnemies[type] is true, which we set in this class above
+                NetMessage.SendData(MessageID.SpawnBoss, number: player.whoAmI, number2: randomBoss);
+            }
+        }
 	}
 }
